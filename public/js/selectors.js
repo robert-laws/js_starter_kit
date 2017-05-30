e36e69ac07a0716402dd129ebbc7e37a8dee4bb7
Robert Laws <roblaws@me.com>
2017-05-30 16:41:48 +0300

    done jquery lab

---
+++ public/js/selectors.js
@@ -0,0 +1,24 @@
+function paragraphSelector() {
+  var paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last')
+  return lastImg
+}
+
+function ninjaBabySelector() {
+  var ninja = $('#baby-ninja')
+  return ninja
+}
+
+function divSelector() {
+  var picDivs = $('div.pics')
+  return picDivs
+}
+
+function firstListItem() {
+  var firstLI = $('ul.pic-list li:first-child')
+  return firstLI
+}
